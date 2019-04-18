626c4c3c3c862e2ae25e1fcfe9683a97e5275001
ThePriest1 <stas.shilovich@gmail.com>
2019-04-18 15:39:07 +0300

    root

---
+++ src/main/java/com/gmail/shilovich/stas/jd2_project_week3/Jd2ProjectWeek3Application.java
@@ -0,0 +1,13 @@
+package com.gmail.shilovich.stas.jd2_project_week3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jd2ProjectWeek3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jd2ProjectWeek3Application.class, args);
+	}
+
+}
